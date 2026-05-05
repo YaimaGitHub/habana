11c895be44889fb6e1f1bd34e2bae8c66c6c5db8
YaimaGitHub <tvalacarta33@gmail.com>
2026-05-05 03:17:36 +0200

    q
    q

--- js/app-data.js
+++ js/app-data.js
@@ -2,7 +2,7 @@
  * D'Mima - Datos Compartidos
  * Este archivo contiene los datos de configuracion que son compartidos
  * entre la tienda (index.html) y el panel de control (admin.html)
- * Generado desde el Panel de Control: 5/5/2026, 2:53:45
+ * Generado desde el Panel de Control: 5/5/2026, 3:16:08
  */
 
 // Numero de WhatsApp para pedidos (sin el +)
@@ -14,20 +14,23 @@ var MEU_ENDERECO = null;
 // Municipios de La Habana con costo de envio (en MN / CUP)
 var MUNICIPIOS_HABANA = [
     { id: 'Guanabacoa', nome: 'Guanabacoa (Nalón - Mikito - Hata - Roble)', costo: 1000 },
-    { id: 'centro-habana', nome: 'Centro Habana', costo: 200 },
-    { id: 'plaza', nome: 'Plaza de la Revolución', costo: 250 },
+    { id: 'centro-habana', nome: 'Centro Habana', costo: 2000 },
+    { id: 'Guanabacoa', nome: 'Guanabacoa (Calzada vieja)', costo: 500 },
     { id: 'cerro', nome: 'Cerro', costo: 250 },
-    { id: 'diez-de-octubre', nome: 'Diez de Octubre', costo: 250 },
+    { id: 'diez-de-octubre', nome: 'Diez de Octubre', costo: 2500 },
     { id: 'playa', nome: 'Playa', costo: 350 },
     { id: 'Guanabacoa', nome: 'Guanabacoa (Reparto Chiva)', costo: 400 },
     { id: 'Guanabacoa', nome: 'Guanabacoa (Parque - Roble)', costo: 700 },
     { id: 'Guanabacoa', nome: 'Guanabacoa Machaco - Mikito', costo: 700 },
-    { id: 'arroyo-naranjo', nome: 'Arroyo Naranjo', costo: 400 },
+    { id: 'Guanabacoa', nome: 'Reparto Mañana', costo: 450 },
     { id: 'san-miguel', nome: 'San Miguel del Padrón', costo: 2500 },
     { id: 'guanabacoa ', nome: 'Guanabacoa (Semáforo-Machaco)', costo: 500 },
     { id: 'regla', nome: 'Regla', costo: 200 },
-    { id: 'habana-del-este', nome: 'Habana del Este', costo: 450 },
-    { id: 'cotorro', nome: 'Cotorro', costo: 500 }
+    { id: 'habana-del-este', nome: 'Habana del Este (Bahía)', costo: 1000 },
+    { id: 'Guanabacoa', nome: 'Guanabacoa (Habana nueva)', costo: 300 },
+    { id: 'Vedado', nome: 'Vedado', costo: 2500 },
+    { id: 'Villa Panamericana', nome: 'Villa Panamericana', costo: 1500 },
+    { id: 'Reparto Camilo Cienfuegos', nome: 'Reparto Camilo Cienfuegos', costo: 2000 }
 ];
 
 // Metadata de las categorias: nombre visible, icono y clave interna
