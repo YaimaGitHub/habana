2e8e0e021c1ee3e18a264fab516dcf400a3c9112
YaimaGitHub <tvalacarta33@gmail.com>
2026-05-06 20:18:50 -0400

    u
    km

--- js/js/app-data.js
+++ js/js/app-data.js
@@ -2,7 +2,7 @@
  * D'Mima - Datos Compartidos
  * Este archivo contiene los datos de configuracion que son compartidos
  * entre la tienda (index.html) y el panel de control (admin.html)
- * Generado desde el Panel de Control: 6/5/2026, 10:29:12 a.m.
+ * Generado desde el Panel de Control: 6/5/2026, 8:17:20 p.m.
  */
 
 // Numero de WhatsApp para pedidos (sin el +)
